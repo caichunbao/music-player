0cd5f1ec5272055e52b7023bc652ced66696fcdf
caichunbao <vipcaichunbao@163.com>
2018-04-27 16:03:05 +0800

    add lib

--- gruntfile.js
+++ gruntfile.js
@@ -1,97 +1,97 @@
 /**
- * 
+ * 项目配置文件
  * 此文件是node加载，所以模块语法使用node语法
  * 
  */
 
-module.exports = function(grunt){
+module.exports = function(grunt) {
   grunt.initConfig({
-    cssmin:{
-      target:{
-        files:[{
-          expand:true,
-          cwd:"./public/css",
-          src:["*.css","!*.min.css"],
-          dest:"./build/css",
-          ext:".min.css",
-          rename:function(src,name){
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: "./public/css",
+          src: ["*.css", "!*.min.css"],
+          dest: "./build/css",
+          ext: ".min.css",
+          rename: function(src, name) {
             return src + "/" + name
           },
         }]
       }
     },
-    uglify:{
-      target:{
+    uglify: {
+      target: {
         options: {
           compress: {
             drop_console: false
           },
-          sourceMap:true
+          sourceMap: true
         },
-        files:[{
-          expand:true,
-          cwd:"./build/js",
-          src:["*.js","!.min.js"],
-          dest:"./build/js",
-          ext:".min.js",
-          rename:function(src,name){
+        files: [{
+          expand: true,
+          cwd: "./build/js",
+          src: ["*.js", "!*.min.js"],
+          dest: "./build/js",
+          ext: ".min.js",
+          rename: function(src, name) {
             return src + "/" + name
           },
         }]
       }
     },
-    babel:{
-      options:{
-        sourceMap:false,
-        presets:["env"]
+    babel: {
+      options: {
+        sourceMap: false,
+        presets: ["env"]
       },
-      dist:{
+      dist: {
         files: [{
-          expand:true,
-          cwd:'public/js', //js目录下
-          src:['*.js'],    //所有js文件
-          dest:'build/js'  //输出到此目录下
-        }] 
+          expand: true,
+          cwd: 'public/js', //js目录下
+          src: ['*.js'], //所有js文件
+          dest: 'build/js' //输出到此目录下
+        }]
       }
     },
-    imagemin:{
-      dynamic:{
-        files:[{
-          expand:true,
-          cwd:"./public/img",
-          src:["**/*.{png,jpg,gif}"],
-          dest:"./build/img"
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: "./public/img",
+          src: ["**/*.{png,jpg,gif}"],
+          dest: "./build/img"
         }],
-        options:{
-          optimizationLevel:3
+        options: {
+          optimizationLevel: 3
         }
       }
     },
-    watch:{
-      es6:{
-        files:"./public/js/*.js",
-        tasks:["clean","babel"],
-        options:{
-          livereload:true
+    watch: {
+      es6: {
+        files: "./public/js/*.js",
+        tasks: ["clean", "babel"],
+        options: {
+          livereload: true
         }
       },
-      js:{
-        files:["./build/js/*.js","!./build/js/*.min.js"],
-        tasks:["uglify"],
-        options:{
-          livereload:true
+      js: {
+        files: ["./build/js/*.js", "!./build/js/*.min.js"],
+        tasks: ["uglify"],
+        options: {
+          livereload: true
         }
       },
-      css:{
-        files:"./public/css/*.css",
-        tasks:["cssmin"],
-        options:{
-          livereload:true
+      css: {
+        files: "./public/css/*.css",
+        tasks: ["cssmin"],
+        options: {
+          livereload: true
         }
       }
     }
   })
-  grunt.registerTask("clean",function(){
+  grunt.registerTask("clean", function() {
     grunt.file.delete("build/js")
   })
   grunt.loadNpmTasks('grunt-babel');
